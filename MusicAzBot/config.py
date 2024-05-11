851ec650239c2b6bb484199abb9158bc7e7c31dd
feridoffical <110368464+feridoffical@users.noreply.github.com>
2024-05-12 00:12:54 +0400

    Update config.py

--- MusicAzBot/config.py
+++ MusicAzBot/config.py
@@ -6,7 +6,7 @@ class Config:
    API_ID = int(os.getenv("API_ID", "21880980"))
    API_HASH = os.getenv("API_HASH", "17793cd64a21c800fdba74b7962b659e")
    BOT_TOKEN = os.getenv("BOT_TOKEN", "7190413281:AAGjRpoPVqo40z4B_9Qpo-DmsasBMLJYZt0")
-   BOT_USERNAME = os.environ.get("BOT_USERNAME", EfuMusic")
+   BOT_USERNAME = os.environ.get("BOT_USERNAME", "EfuMusic")
    OWNER_NAME = os.environ.get("OWNER_NAME", "Feridoffical") 
    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "EfuMusicPlaylist")
    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1002098118049"))
